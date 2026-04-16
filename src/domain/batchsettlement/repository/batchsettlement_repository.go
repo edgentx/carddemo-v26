75010ba3f30fb6e6dae621539130b3d88b294e95
Eric Diana <taylormade7978@hotmail.com>
2026-04-16 11:40:14 -0500

    feat(feature/s-1): src/domain/batchsettlement/repository/batchsettlement_repository.go

---
+++ src/domain/batchsettlement/repository/batchsettlement_repository.go
@@ -0,0 +1,11 @@
+package repository
+
+import "github.com/carddemo/project/src/domain/batchsettlement/model"
+
+// BatchSettlementRepository defines the storage interface for BatchSettlement aggregates.
+type BatchSettlementRepository interface {
+	Get(id string) (*model.BatchSettlement, error)
+	Save(aggregate *model.BatchSettlement) error
+	Delete(id string) error
+	List() ([]*model.BatchSettlement, error)
+}
